4f7e60a590a214473a51b260c64b24e3d08c256d
Carlos <carlosdaniloem@gmail.com>
2024-07-17 23:58:31 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-websocket-yi7z.onrender.com")
     websocket.onmessage = processMessage
 }
 
